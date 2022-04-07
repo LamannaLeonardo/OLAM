dbdc29684f7e19ea7ff2c573226b3cc805e3dc1a
LamannaLeonardo <leonardo.lamanna.75@gmail.com>
2022-04-07 14:43:14 +0200

    Add license

--- Util/Latex_generator.py
+++ Util/Latex_generator.py
@@ -1,3 +1,9 @@
+# Copyright (c) 2022, Leonardo Lamanna
+# All rights reserved.
+# This source code is licensed under the MIT-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+
 import pandas as pd
 import os
 
